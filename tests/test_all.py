5b5fa3129529251444769bc55a6db38b72e16842
kaho <kaho0769@qq.com>
2024-04-01 04:01:59 +0800

    chore: merge branch 'alpha'

--- tests/test_all.py
+++ tests/test_all.py
@@ -2,5 +2,6 @@ import pytest
 import aeipathy
 
 def test_all():
-    assert aeipathy.hello() == "Hello, world!"
+    assert aeipathy.hello("Alice") == "Hello, Alice!"
     assert aeipathy.add(1, 2) == 3
+    assert aeipathy.multiply(2, 3) == 6
